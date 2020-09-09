c669a49ed0875cd83a1c6d9c242a8acd54ae454b
Rick Battagline <rick@embed.com>
2020-09-09 14:19:16 -0600

    Adding terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
+import { terser } from "rollup-plugin-terser";
 export default {
   input: 'src/AsteroidsGame.js',
+  plugins: [terser()],
   output: {
     file: 'build/asteroidshooter.min.js',
     format: 'esm', // 'iife',
